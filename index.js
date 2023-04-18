7609a2d231c467a4ac30bef0a400b895d6b4198b
Darlley Brito <darlleybrito@gmail.com>
2023-04-18 17:58:03 -0300

    design in work

--- index.js
+++ index.js
@@ -6,26 +6,54 @@ const port = 3000
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+// C L I E N T
 app.get('/', (req, res) => {
   res.render('home', {
-    appName: 'Darlley Dev - Currículo'
+    appName: 'Darlley Brito - Currículo',
+    appDescription: 'Sou Desenvolvedor Web Full Stack Júnior, UI Designer, SEO e AWS Cloud Pratictioner, veja meu currículo.'
   })
 })
-app.get('/experiencia', (req, res) => {
-  res.render('experiencia', {
+
+// A D M I N 
+app.get('/admin', (req, res) => {
+  res.render('admin/dashboard', {
     appName: 'Darlley Dev - Experiencia'
   })
 })
 
-app.get('/projeto', (req, res) => {
-  res.render('projeto', {
+app.get('/admin/contact', (req, res) => {
+  res.render('admin/contact', {
     appName: 'Darlley Dev - Projeto'
   })
 })
 
-app.get('/curso', (req, res) => {
-  res.render('curso', {
-    appName: 'Darlley Dev - Curso'
+app.get('/admin/profile', (req, res) => {
+  res.render('admin/profile', {
+    appName: 'Darlley Dev - Projeto'
+  })
+})
+
+app.get('/admin/education', (req, res) => {
+  res.render('admin/education', {
+    appName: 'Darlley Dev - Projeto'
+  })
+})
+
+app.get('/admin/experience', (req, res) => {
+  res.render('admin/experience', {
+    appName: 'Darlley Dev - Projeto'
+  })
+})
+
+app.get('/admin/skills', (req, res) => {
+  res.render('admin/skills', {
+    appName: 'Darlley Dev - Projeto'
+  })
+})
+
+app.get('/admin/languages', (req, res) => {
+  res.render('admin/languages', {
+    appName: 'Darlley Dev - Projeto'
   })
 })
 
